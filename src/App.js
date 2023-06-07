9308974a853ef155b2ba3dcb5204f4454daba7ae
MuchiriAndrew <kariukia225@gmail.com>
2023-06-07 11:56:14 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './custom.scss';
+function App() {
+  return(
+    <>
+    <p className="text-primary">This is some random text</p>
+    <p className="text-secondary">This is some random text</p>
+    <p className="text-dark">This is some random text</p>
+
+    </>
+
+  );
+}
+
+export default App;
